fa39105c69adb79b42bc757b2e6c348f57a3bcff
unknown <Supadon004@gmail.com>
2022-08-07 14:41:12 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<script >
+import { ref } from '@vue/reactivity'
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name:"Home",
+  components: {
+    HelloWorld,
+  },
+  setup() {
+    let dropzoneFile = ref("");
+
+    const drop = (e) => {
+      dropzoneFile.value = e.dataTransfer.files[0]
+    }
+
+    return {dropzoneFile, drop };
+  }
+}
+</script>
+
+<template>
+<div class="home">
+<h1>Dropzone</h1>
+<HelloWorld @drop.prevent="drop"/>
+  <span class="file-info">File: {{dropzoneFile.name}}</span>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.home {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color:#f1f1f1;
+
+  h1 {
+    font-size: 40px;
+    margin-bottom:32px ;
+  }
+
+  .file-info {
+    margin-top: 32px;
+  }
+}
+
+</style>
