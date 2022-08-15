13b4ea38bd2ecac3242328b8e57d74a5f34e0456
supadon <Supadon004@gmail.com>
2022-08-16 00:11:18 +0700

    Edit8

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -4,6 +4,23 @@
       <div class="hello" style="padding-top: 20px">
       </div>
 
+      <div class="hello" style="background-color:#def8f6; padding-top: 20px;">    
+    <picture-input 
+      ref="pictureInput"
+      width="700" 
+      height="350" 
+      margin="16" 
+      accept="image/jpeg,image/png" 
+      size="10" 
+      button-class="btn"
+      :custom-strings="{
+        upload: '<h1>Bummer!</h1>',
+        drag: 'Drag & Drop'
+      }"
+      @change="onChange">
+    </picture-input>
+  </div>
+
       <div class="mt-4 d-grid gap-5">
         <!-- form -->
         <div class="form">
@@ -24,18 +41,17 @@
           <option value="RIGHT">RIGHT</option>
         </select>
 
-        <div class="date">
-          <Datepicker
-            v-model="date"
-            :enableTimePicker="false"
-            placeholder="Date :"
-          />
-        </div>
+        
+        <!-- datepicker -->
+          <div>
+            <Datepicker lang="en" position="right"/>
+          </div>
+
       </div>
         <!-- button -->
         <div class="d-grid gap-3 d-md-flex justify-content-md-end mt-5">
           <button type="button" class="btn btn-default">CLEAR</button>
-          <button  class="btn btn-primary">
+          <button  class="btn btn-primary" @click="upload">
             PREDICT
           </button>
         </div>
@@ -45,25 +61,82 @@
 </template>
 
 <script>
-import Datepicker from "@vuepic/vue-datepicker";
-import "@vuepic/vue-datepicker/dist/main.css";
+import 'vue-datepicker-ui/lib/vuedatepickerui.css';
+import VueDatepickerUi from 'vue-datepicker-ui';
+import axios from "axios";
+import PictureInput from 'vue-picture-input';
 
 export default {
   name: "Form",
-  components: { Datepicker },
+  components: { Datepicker: VueDatepickerUi, PictureInput },
+  setup() {
+
+    function upload(){
+      let formData = new Formdata()
+      formData.append('image', images)
+      let imageController = axios.create({
+        baseURL: 'http://127.0.0.1/api/uploads'
+      })
+      console.log(images)
+      
+              
+        return new Promise(async (resolve, reject) => {
+        try {
+          const res = await imageController.post(
+            formData,
+          )
+          resolve(res.data)
+        } catch(e){
+          reject(e.response.data)
+        }
+      })
+      };
+    function upload()
+            {
+                  let name,email,psw;
+                  name=document.getElementById("username").value;
+                  email=document.getElementById("eyeside").value;
+                  psw=document.getElementById("date").value;
+
+                  localStorage.setItem("name",name)
+                  localStorage.setItem("eyeside",email)
+                  localStorage.setItem("date",psw)
+                
+
+                  };
+      function clearitem() {
+        localStorage.clear();
+      }
+
+  return {
+      date: null,
+      upload,
+    };
+    
+},
   data() {
     return {
-      date: null,
+      date: {}, dateGreg:{}
     };
   },
-
+  methods: {
+    onChange (image) {
+      console.log('New picture selected!')
+      if (image) {
+        console.log('Picture loaded.')
+        this.image = image
+      } else {
+        console.log('FileReader API not supported: use the <form>, Luke!')
+      }
+    }
+  }
 }
 </script>
 
 <style scoped lang="scss">
 .container {
   width: 50% !important;
-  height: 100vh;
+  height: 130vh;
 }
 
 .btn-default {
@@ -93,4 +166,5 @@ option[value=""][disabled] {
 option {
   color: black;
 }
+
 </style>
